ee0d8f7f118aa4fba55f92025ed2f70f171d2da9
Anton Yeshchenko <anton.yeshchenko@gmail.com>
2021-02-24 00:15:44 +0100

    connect dfg to brushes first level

--- vis-system/dfg/main-dfg.js
+++ vis-system/dfg/main-dfg.js
@@ -8,21 +8,28 @@ function configDFG(){
     g.graph().Schedule = true;
     g.graph().WBS = false;
     console.log(g)  
-    return g;
+
+    config = {}
+    config.g = g
+    config.threshold_arc_min = 150 
+    return config;
 }
 
 //function to draw dfg on 
-function drawDFG(g, data){
+function drawDFG(data, config_dfg){
+    console.log(data)
     console.log("dfg drawing start")
+
     let states = {}
     for (let j = 0 ; j < data.dfrs.length ; j+= 1){
         states[data.dfrs[j].act1] = {
             description: "description"
             }
-            let temp_sum = d3.sum(data.dfrs[j].series) 
-            if (temp_sum > 150){
+            let temp_sum = data.dfrs[j].series_sum_each_arc;
+
+            if (temp_sum > config_dfg.threshold_arc_min){
                 // // Set up the edges
-                g.setEdge(data.dfrs[j].act1, data.dfrs[j].act2, 
+                config_dfg.g.setEdge(data.dfrs[j].act1, data.dfrs[j].act2, 
                     {
                         curve: d3.curveBasis, // cuvre the edges
                         label: temp_sum
@@ -39,7 +46,6 @@ function drawDFG(g, data){
             }
     }
 
-    console.log('at the end of the draw DFG section4')
     console.log(states)
 
     // Add states to the graph, set labels, and style
@@ -47,16 +53,17 @@ function drawDFG(g, data){
         var value = states[state];
         value.label = state;
         value.rx = value.ry = 5;
-        g.setNode(state, value);
+        config_dfg.g.setNode(state, value);
     });
     
-    console.log('at the end of the draw DFG section3')
     // Create the renderer
     var render = new dagreD3.render();
     // render.edgeTension('linear')
     // Set up an SVG group so that we can translate the final graph.
-    var svg = d3.select("#DFGChart"),
-        inner = svg.append("g");
+
+    var svg = d3.select("#DFGChart");
+    let inner = svg.append("g");
+
     // Set up zoom support
     var zoom = d3.zoom()
         .on("zoom", function(event) {
@@ -68,20 +75,19 @@ function drawDFG(g, data){
     var styleTooltip = function(name, description) {
         return "<p class='name'>" + name + "</p><p class='description'>" + description + "</p>";
     };
-    console.log('at the end of the draw DFG section2')
     
 
     // Run the renderer. This is what draws the final graph.
-    render(inner, g);
+    render(inner, config_dfg.g);
     
-    inner.selectAll("g.node")
-        .attr("title", function(v) { return styleTooltip(v, g.node(v).description) })
+    inner.selectAll("config_lineplot.g.node")
+        .attr("title", function(v) { return styleTooltip(v, config_dfg.g.node(v).description) })
         .each(function(v) { $(this).tipsy({ gravity: "w", opacity: 1, html: true}); });
     
     // Center the graph
     var initialScale = 0.75;
-    svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr("width") - g.graph().width * initialScale) / 2, 20).scale(initialScale));
-    svg.attr('height', g.graph().height * initialScale + 40);
-    console.log('at the end of the draw DFG section');
-
+    svg.call(zoom.transform, d3.zoomIdentity.translate((svg.attr("width") - config_dfg.g.graph().width * initialScale) / 2, 20).scale(initialScale));
+    svg.attr('height', config_dfg.g.graph().height * initialScale + 40);
 }
+
+
--- vis-system/index.js
+++ vis-system/index.js
@@ -1,6 +1,7 @@
 // in this file the main system is set up to be running
 
-dfg = configDFG();
+config_dfg = configDFG();
+config_lineplot = configLineplot()
 
 // let count = 50
 // let count_actual = 25
@@ -10,11 +11,11 @@ data.count_actual = 25
 // data.timestamps - timestamps
 // data.dfrs - directly follow relations for each constraints
 // data.dfrs[].series[]
-// data.dfrs.act1
-// data.dfrs.act2
-// data.dfrs.technique
+// data.dfrs[].act1
+// data.dfrs[].act2
+// data.dfrs[].technique
 data.dfrs = []
-
+data.series_sum = new Array(data.count).fill(0)// this is for the sum of the time series to build the graph
 
 d3.csv("bpi12-50-25-25.csv",
   // When reading the csv, I must format variables:
@@ -25,7 +26,7 @@ d3.csv("bpi12-50-25-25.csv",
             for (let j = 0 ; j < data.count ; j+=1){
                 // https://github.com/d3/d3-time-format
                 let temp_date = d3.timeParse("%Y-%m-%d %H:%M:%S.%f%Z")(d[j])
-                timestamps.push(temp_date)    
+                timestamps.push(temp_date) 
             }
             // should we add here also unnecessary columns?
             data.timestamps = timestamps;
@@ -35,21 +36,102 @@ d3.csv("bpi12-50-25-25.csv",
             temp = []
             temp.series = []
             for (let j = 0 ; j < data.count ; j+=1){
-                temp.series.push(+d[j])
+                let temp_v = +d[j]
+                temp.series.push(temp_v)
+                // calculate additional timeseries for line chart
+                data.series_sum[j] += temp_v
             }
             temp.act1 = d.act1
             temp.act2 = d.act2
             temp.technique = d.technique
+            // add comdined info about the arcs
+            temp.series_sum_each_arc = d3.sum(temp.series)
             data.dfrs.push(temp)
             // return dfrs;
         }
     })
     .then( (d,i) => {
-        console.log("dfg")
-        console.log(dfg)
-        drawDFG(dfg, data)
+        console.log("---------dfg")
+        console.log(data)
+        drawDFG(data, config_dfg)
+        drawLineplot(data, config_lineplot)
 
 })
 .catch(function(error){
   console.log('cannot import file')
 })
+
+// this function receives the selections of the 1-2 brushes of the linechart
+// it then updates the dfg 
+function updateSelection(selections_dates){
+    console.log(selections_dates)
+    // filter data    
+    // for the case of one brush only
+    if (selections_dates.length === 1){
+        new_data_1 = filterDataByDate(selections_dates[0])
+
+        // initialize the dfg againt
+        config_dfg = configDFG();
+
+        // remove the previous plot
+        d3.select("#DFGChart").selectAll("*").remove()
+        
+        // draw new plot
+        drawDFG(new_data_1, config_dfg)
+    }
+    else { // here is when two regions are brushed
+        // new_data_1 = filterDataByDate(selections_dates[0])
+        // new_data_2 = filterDataByDate(selections_dates[1])
+
+        // console.log(new_data_1)
+        // // initialize the dfg againt
+        // config_dfg = configDFG();
+
+        // // remove the previous plot
+        // d3.select("#DFGChart").selectAll("*").remove()
+        
+        // // draw new plot
+        // // drawDFGDiff(new_data_1, new_data_2, config_dfg)
+        // // drawDFG(new_data_1, config_dfg)
+    }
+}
+
+
+
+// this function filters the dataset by the dates, and returns the complete new set of datavalues from that filtered region
+function filterDataByDate(dates){
+    let temp_data = {}
+    temp_data.dfrs = []
+
+    console.log("filter function")
+
+    console.log(data)
+    temp_data.timestamps = data.timestamps.filter(function (t) {
+        return t > dates[0] && t < dates[1]
+    })
+
+    let temp_timestamp_in_range_first = 0
+    while (data.timestamps[temp_timestamp_in_range_first] < dates[0]){
+        temp_timestamp_in_range_first += 1
+    }
+    let temp_timestamp_in_range_second = temp_timestamp_in_range_first + temp_data.timestamps.length
+
+    console.log(temp_timestamp_in_range_first)
+    console.log(temp_timestamp_in_range_second)
+
+    for (let elem of data.dfrs){
+        temp = []
+        temp.series = []
+        for(let j = temp_timestamp_in_range_first ; j < temp_timestamp_in_range_second ; j+=1){
+            temp.series.push(elem.series[j])
+        }
+        temp.act1 = elem.act1
+        temp.act2 = elem.act2
+        temp.technique = elem.technique
+        temp.series_sum_each_arc = d3.sum(temp.series)
+        temp_data.dfrs.push(temp) 
+    }
+
+    console.log(temp_data)
+    return temp_data;
+}
